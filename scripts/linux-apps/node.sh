fb74a3ce2a3f6619dc4d079315186a1d14068459
Rich Barlow <rsb177@hotmail.com>
2026-04-18 22:42:05 -0400

    Install latest LTS Node as global version via fnm

--- scripts/linux-apps/node.sh
+++ scripts/linux-apps/node.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "📦 Installing fnm"
+echo "Installing fnm"
 curl -fsSL https://fnm.vercel.app/install | bash
+
+export PATH="$HOME/.local/share/fnm:$PATH"
+eval "$(fnm env)"
+
+echo "Installing latest LTS Node as global version"
+fnm install --lts
+fnm default lts-latest
