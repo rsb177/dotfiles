06c81187a043e6ecef1cb306af98a2c61a7b7c60
Rich Barlow <rsb177@hotmail.com>
2026-04-20 15:11:35 -0400

    Prompt for email type during git config setup

--- scripts/linux-apps/git_config.sh
+++ scripts/linux-apps/git_config.sh
@@ -2,3 +2,11 @@
 
 git config --global user.name "Rich Barlow"
 git config --global --add --bool push.autoSetupRemote true
+
+read -rp "Is this a work machine? [y/N] " work_machine
+if [[ "$work_machine" =~ ^[Yy]$ ]]; then
+    read -rp "Enter work email: " work_email
+    git config --global user.email "$work_email"
+else
+    git config --global user.email "rsb177@hotmail.com"
+fi
