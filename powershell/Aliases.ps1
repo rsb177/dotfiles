8f6bc1854d95e21515f6e2963394c125200be3bf
Rich Barlow <rsb177@hotmail.com>
2026-04-20 22:02:55 -0400

    remove poetry from Aliases.ps1

--- powershell/Aliases.ps1
+++ powershell/Aliases.ps1
@@ -12,12 +12,3 @@ if (Get-Command Remove-Alias -ErrorAction SilentlyContinue) {
 }
 Function ll { eza -lbGhgF --color always --icons -a --git @args }
 Function ls { eza -lbGhgF --color auto --icons -a -s type --git @args }
-
-# Poetry
-function poetry_shell() {
-    $envInfo = ($envInfo | Select-String -Pattern "Path:\s+(?<path>\S+)")
-    $venvPath = $envInfo.Matches.Groups[1].Value + "\Scripts\Activate.ps1"
-    . $venvPath
-}
-
-New-Alias -Name pshell -Value poetry_shell -Force
